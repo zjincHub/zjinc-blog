fc62aa3a2273426859131b357b284547c911cb2e
zjincHub <1365815077@qq.com>
2024-10-28 23:07:58 +0800

    feat: code组件实现多文件

--- src/app/_components/code/index.tsx
+++ src/app/_components/code/index.tsx
@@ -3,17 +3,21 @@ import React, { useEffect, useState } from 'react';
 
 interface Props {
   lang: string;
-  name: string;
+  names: string;
   theme?: string;
   children: any;
 }
 
-// 直接用 import('shiki') 会报错，所以这里用动态 import
+/**
+ * 直接用 import('shiki') 会报错，所以这里用动态 import
+ */
 const importShiki = async () => {
   return await import('shiki');
 };
 
-// 根据props.children拼接成完整的string
+/**
+ * 根据props.children拼接成完整的string
+ */
 const codeFormat = (props: any) => {
   const children = props.children;
   // 如果没有children，直接返回
@@ -36,15 +40,45 @@ const codeFormat = (props: any) => {
   return '';
 };
 
+/**
+ * 切割html
+ */
+const cutHtml = (html: string) => {
+  // 提取html的头、尾、内容
+  const headRegex = /<pre\b[^>]*><code>/i;
+  const contentRegex = /<pre[^>]*><code>([\s\S]*?)<\/code><\/pre>/;
+  const head = html.match(headRegex)?.[0] || '';
+  const foot = '</code></pre>';
+  const content = html.match(contentRegex)?.[1] || '';
+  // 提取分割线代码
+  const dividingRegex =
+    /\n<span class="line"><span\b[^>]*>--<\/span><\/span>\n/;
+  const dividing = content.match(dividingRegex)?.[0] || '';
+  // 用分割线代码把html的content分割成不同文件
+  if (dividing) {
+    const contentList = content.split(dividing);
+    return contentList.map((cont) => head + cont + foot);
+  } else {
+    return [head + content + foot];
+  }
+};
+
 export default function Index(props: Props) {
-  const { lang, theme = 'vitesse-dark' } = props;
-  const [code, setCode] = useState('');
+  const { lang, theme = 'vitesse-dark', names = '' } = props;
+  // 文件名称数组
+  const nameList = names.split(',');
+  // 用于计算html的string，由code计算得到
   const codeString = codeFormat(props);
+  // 需要展示的html，由code计算得到
+  const [codeHtmlList, setCodeHtmlList] = useState<string[]>([]);
+  // 当前展示的html的index
+  const [showIndex, setShowIndex] = useState(0);
 
   const getHtmlByCode = async () => {
     const { codeToHtml } = await importShiki();
     const html = await codeToHtml(codeString, { lang, theme });
-    setCode(html);
+    const htmlList = cutHtml(html);
+    setCodeHtmlList(htmlList);
   };
 
   useEffect(() => {
@@ -65,9 +99,21 @@ export default function Index(props: Props) {
             <div className="w-3 h-3 rounded-full bg-green-500"></div>
           </div>
         </div>
-        <div className="text-white text-sm">index.js</div>
+        <div className="flex items-center">
+          {nameList.map((name, index) => (
+            <div
+              key={name}
+              className="text-white text-sm"
+              onClick={() => setShowIndex(index)}>
+              {name}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="px-5 pb-4" dangerouslySetInnerHTML={{ __html: code }} />
+      <div
+        className="px-5 pb-4"
+        dangerouslySetInnerHTML={{ __html: codeHtmlList[showIndex] || '' }}
+      />
     </div>
   );
 }
