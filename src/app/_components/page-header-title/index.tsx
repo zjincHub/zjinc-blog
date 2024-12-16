9c198bf4c1493f8a970a2e11bad714b47219eb39
zjincHub <1365815077@qq.com>
2024-12-16 23:18:53 +0800

    feat: code-beautify文章更新

--- src/app/_components/page-header-title/index.tsx
+++ src/app/_components/page-header-title/index.tsx
@@ -1,6 +1,6 @@
 export default function Index(props: IProps) {
   return (
-    <div className="relative max-w-[800px] m-auto px-4 box-border">
+    <div className="relative max-w-[1180px] m-auto px-12 box-border">
       <h1 className="mt-0 text-sky-800 font-bold text-3xl">{props.title}</h1>
       <div className="mt-4 mb-10 text-sm">
         <span>{`作者：${props.author}`}</span>
