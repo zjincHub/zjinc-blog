01ef906f30eccf0c20a5a68717edebc293a11051
章津诚 <52648752+zjincHub@users.noreply.github.com>
2025-01-14 20:04:11 +0800

    feat: blog-cover组件样式优化

--- src/app/_components/blog-cover/index.tsx
+++ src/app/_components/blog-cover/index.tsx
@@ -6,11 +6,13 @@ import './index.scss';
 export default function Index(props: IProps) {
   return (
     <div className="blog-cover">
+      {/* 标题 */}
       <span className="title-wrapper">
         <Link className="title-link" href={props.path}>
           {props.title}
         </Link>
       </span>
+      {/* 博客标签 */}
       <div className="tags-wrapper">
         {props.tags.map((tag, index) => (
           <span key={index} className="tag-item">
@@ -19,9 +21,28 @@ export default function Index(props: IProps) {
           </span>
         ))}
       </div>
+      {/* 博客简介 */}
       <div className="summary-wrapper">{props.summary}</div>
+      {/* 底部按钮 */}
       <div className="foot">
         <ButtonReadMore path={props.path} />
+      </div>
+      {/* 底部分割线 */}
+      <div className="wave-wrapper">
+        <svg
+          className="wave-divider"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,60 C200,30 400,90 600,60 C800,30 1000,90 1200,60"
+            fill="none"
+            stroke="var(--color-text-decorate)"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
     </div>
   );
