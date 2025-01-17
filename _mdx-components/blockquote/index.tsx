d69ff01a0037b6ba77969b73c0315eaa19818b9f
章津诚 <52648752+zjincHub@users.noreply.github.com>
2025-01-17 18:33:29 +0800

    feat: blockquote组件根据mode区分样式，未完成

--- _mdx-components/blockquote/index.tsx
+++ _mdx-components/blockquote/index.tsx
@@ -1,6 +1,13 @@
 import H1 from '../h1';
 import P from '../p';
+import UL from '../ul';
 import './index.scss';
+
+enum EnumMode {
+  Primary = 'primary',
+  Danger = 'danger',
+  Warning = 'warning',
+}
 
 const renderH1 = (string: string, index: number) => {
   return (
@@ -18,13 +25,26 @@ const renderP = (string: string, index: number) => {
   );
 };
 
+const computeMode = (originalChildren: any[]) => {
+  const hasModeChild = originalChildren.find(
+    (item: any) => item?.props?.children
+  );
+  const mode = hasModeChild?.props?.children?.trim() || '';
+  const isModeInclude = Object.values(EnumMode).includes(mode);
+  return isModeInclude ? mode : EnumMode.Primary;
+};
+
 export default function Blockquote(props: { children?: any }): JSX.Element {
   const children: any = [];
   props.children.forEach((child: any, index: number) => {
+    const originalChildren = child?.props?.children;
     if (child.type === H1) {
-      children.push(renderH1(child.props.children, index));
+      children.push(renderH1(originalChildren, index));
     } else if (child.type === P) {
-      children.push(renderP(child.props.children, index));
+      children.push(renderP(originalChildren, index));
+    } else if (child.type === UL) {
+      const mode = computeMode(originalChildren);
+      // TODO 不用mode不同样式
     }
   });
   return <blockquote className="blockquote">{children}</blockquote>;
